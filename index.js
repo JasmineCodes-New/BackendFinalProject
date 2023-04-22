aecaf65b3361e489d61f6f29db72fd54ed483b70
Jasmine Irvin <jasmine.irvin@ymail.com>
2023-04-21 20:20:22 -0400

    rolling back changes

--- index.js
+++ index.js
@@ -4,17 +4,31 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 const path = require('path');
 const connectDB = require('./config/dbConnect');
 const routes = require('./routes/states');
-const middlewares = require('./middleware');
+const verifyStateCode = require('./middleware/verifyStateCode');
+const sendHTML = require('./middleware/sendHTML');
+const handle404 = require('./middleware/handle404');
 
 connectDB();
+
+app.use(express.json());
+app.use(cors());
 
 app.use('/', require('./routes/root'));
 app.use('/', routes);
 
-app.use(middlewares);
+app.use(verifyStateCode);
+
+app.use(express.static('public'));
+
+// 404 middleware
+app.use(handle404);
+
+// Root endpoint middleware
+app.use(sendHTML);
 
 app.all('*', (req, res) => {
     res.status(404);
